8585fdf949a5afcceb00721ecf5bd7f36b026d23
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-04-29 19:54:02 +0300

    no thing

--- Flight Booking System/Context/ITIContext.cs
+++ Flight Booking System/Context/ITIContext.cs
@@ -21,13 +21,7 @@ namespace Flight_Booking_System.Context
 
         public DbSet<Ticket> Tickets { get; set; }
 
-        public DbSet<Place> Places { get; set; }
-
-        public DbSet<Country> Countries { get; set; }
-
         public DbSet<Seat> Seats { get; set; }
-
-        public DbSet<State> States { get; set; }
 
 
         public ITIContext(DbContextOptions contextOptions) : base(contextOptions) 
