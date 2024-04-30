f799b44c9a8b2f44322a20cae1e8764ad0d16fbd
ibarhim <ibraihimelfaidy1996@gmail.com>
2024-04-30 19:07:45 +0300

    exttraservices

---
+++ Flight Booking System/Services/StateService.cs
@@ -0,0 +1,56 @@
+﻿using Flight_Booking_System.Models;
+using Flight_Booking_System.Repositories;
+
+namespace Flight_Booking_System.Services
+{
+    public class StateService
+    {
+        private readonly IStateRepository stateRepository;
+
+        public StateService(IStateRepository stateRepository)
+        {
+            this.stateRepository = stateRepository;
+        }
+        //**********************************************
+
+        public List<State> GetAll(string? include = null)
+        {
+            return stateRepository.GetAll(include);
+        }
+
+        public State GetById(int id)
+        {
+            return stateRepository.GetById(id);
+        }
+
+        public List<State> Get(Func<State, bool> where)
+        {
+            return stateRepository.Get(where);
+        }
+
+        public void Insert(State item)
+        {
+            stateRepository.Insert(item);
+            Save();
+        }
+
+        public void Update(State item)
+        {
+            stateRepository.Update(item);
+            Save();
+        }
+
+        public void Delete(State item)
+        {
+            stateRepository.Delete(item);
+            Save();
+        }
+
+        public void Save()
+        {
+            stateRepository.Save();
+        }
+
+        //----------------------------------------
+    }
+}
---
+++ Flight Booking System/Services/Ticketservice.cs
@@ -0,0 +1,58 @@
+﻿using Flight_Booking_System.Models;
+using Flight_Booking_System.Repositories;
+
+namespace Flight_Booking_System.Services
+{
+    public class Ticketservice : ITicketService
+    {
+        private readonly ITicketRepository ticketRepository;
+
+        public Ticketservice(ITicketRepository ticketRepository)
+        {
+            this.ticketRepository = ticketRepository;
+        }
+
+        //**********************************************
+
+        public List<Ticket> GetAll(string? include = null)
+        {
+            return ticketRepository.GetAll(include);
+        }
+
+        public Ticket GetById(int id)
+        {
+            return ticketRepository.GetById(id);
+        }
+
+        public List<Ticket> Get(Func<Ticket, bool> where)
+        {
+            return ticketRepository.Get(where);
+        }
+
+        public void Insert(Ticket item)
+        {
+            ticketRepository.Insert(item);
+            Save();
+        }
+
+        public void Update(Ticket item)
+        {
+            ticketRepository.Update(item);
+            Save();
+        }
+
+        public void Delete(Ticket item)
+        {
+            ticketRepository.Delete(item);
+            Save();
+        }
+
+        public void Save()
+        {
+            ticketRepository.Save();
+        }
+
+        //----------------------------------------
+
+    }
+}
