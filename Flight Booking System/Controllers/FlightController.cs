9e231d43dfa5d2768ea5d97e8b60df598af741ac
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-04-30 15:18:03 +0300

    done with the flight controllers

--- Flight Booking System/Controllers/FlightController.cs
+++ Flight Booking System/Controllers/FlightController.cs
@@ -49,110 +49,188 @@ namespace Flight_Booking_System.Controllers
             return new GeneralResponse()
             {
                 IsSuccess = true,
-                Data = flightDTOs
+                Data = flightDTOs , 
+                Message = "All flights"
             };
         }
 
+        [HttpGet("{id:int}")] // from route 
+        public ActionResult<GeneralResponse> GetbyId(int id)
+        {
+            Flight? flightFromDB = _context.Flights.FirstOrDefault(flight => flight.Id == id);
 
-        //[HttpGet]
-        //public ActionResult<GeneralResponse> GetById()
-        //{
-        //    List<Flight> flights = _context.Flights.ToList();
+            if (flightFromDB == null)
+            {
+                return new GeneralResponse()
+                {
+                    IsSuccess = false,
 
-        //    List<FlightDTO> flightDTOs = new List<FlightDTO>();
+                    Data = null,
 
-        //    foreach (Flight flight in flights)
-        //    {
-        //        FlightDTO flightDTO = new FlightDTO()
-        //        {
-        //            Id = flight.Id,
+                    Message = "No Flight Found with this ID"
+                };
+            }
+            else
+            {
+                FlightDTO flightDTO = new FlightDTO()
+                {
+                    Id = flightFromDB.Id,
 
-        //            Start = flight.Start,
-        //            Destiantion = flight.Destiantion,
+                    Start = flightFromDB.Start,
+                    Destiantion = flightFromDB.Destiantion,
 
-        //            DepartureTime = flight.DepartureTime,
-        //            ArrivalTime = flight.ArrivalTime,
+                    DepartureTime = flightFromDB.DepartureTime,
+                    ArrivalTime = flightFromDB.ArrivalTime,
 
-        //            PlaneId = flight.PlaneId,
-        //        };
+                    PlaneId = flightFromDB.PlaneId,
+                };
 
-        //        flightDTOs.Add(flightDTO);
-        //    }
+                return new GeneralResponse()
+                {
+                    IsSuccess = true,
 
-        //    return new GeneralResponse()
-        //    {
-        //        IsSuccess = true,
-        //        Data = flightDTOs
-        //    };
-        //}
+                    Data = flightDTO,
 
-        //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-
-
-        //[HttpGet("{id:int}")]
-        ////[HttpGet] getting the id from query string only
-        ////[Authorize]
-        //public ActionResult<CategoryDTO> GetById(int id)
-        //{
-        //    Category categoryFromDB = categoryRepository.GetById(id);
-
-        //    if (categoryFromDB == null)
-        //    {
-        //        return BadRequest("No Category found with this ID");
-        //    }
-
-        //    #region manual mapping
-        //    CategoryDTO categoryDTO = new CategoryDTO()
-        //    {
-        //        Id = categoryFromDB.Id,
-        //        Name = categoryFromDB.Name,
-
-        //        ProductsNames = new List<string>(),
-        //    };
-
-        //    foreach (Product prod in categoryFromDB.Products)
-        //    {
-        //        categoryDTO.ProductsNames.Add(new string(prod.Name));
-        //    }
-        //    #endregion
-
-        //    return Ok(categoryDTO);
-        //}
-
-        //[HttpPost]
-        //public IActionResult Add(Category category)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        categoryRepository.Add(category);
-        //        categoryRepository.Save();
-
-        //        return CreatedAtAction("GetById", new { id = category.Id }, category);
-
-        //    }
-        //    return BadRequest(ModelState);
-        //}
-
-        //[HttpPost]
-        //public IActionResult Add(Product product)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        productRepository.Add(product);
-
-        //        productRepository.Save();
-
-        //        // return Ok("Product Added Successfully");
-
-        //        //  return Created($"http://localhost:49076/api/Product/{product.Id}" , product);
-
-        //        return CreatedAtAction("GetById", new { id = product.Id }, product);
-        //    }
-        //    return BadRequest(ModelState);
-        //}
-
-        //&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+                    Message = "Found"
+                };
+            }
+        }
 
 
+        [HttpPost]
+        public ActionResult<GeneralResponse> Add(Flight flight)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(flight);
+
+                _context.SaveChanges();
+
+                return new GeneralResponse()
+                {
+                    IsSuccess = true,
+
+                    Data = flight,
+
+                    Message = "Flight Added Successfully",
+                };
+
+                // omar : we can use one od them if we needed but I think the General Response is better 
+                #region possible return responses
+
+                //return NoContent();
+
+                //return Created($"http://localhost:40640/api/flight{flight.Id}", flight);
+
+                //return CreatedAtAction("GetById", new { id = flight.Id }, flight); 
+                #endregion
+            }
+            else
+            {
+                return new GeneralResponse()
+                {
+                    IsSuccess = false,
+
+                    Data = ModelState,
+
+                    Message = "the Model State is not valid"
+                };
+            }
+        }
+
+        [HttpPut]
+        public ActionResult<GeneralResponse> Edit(int id, Flight editedFlight)
+        {
+            Flight? flightFromDB = _context.Flights.FirstOrDefault(f => f.Id == id);
+
+            if (flightFromDB == null || editedFlight.Id != id)
+            {
+                return new GeneralResponse()
+                {
+                    IsSuccess = false,
+
+                    Data = null,
+
+                    Message = "No Flight Found with this ID or the IDs are not matched , \n" +
+                    " make sure that the original flight ID matches the edited flight ID .",
+                };
+            }
+            else
+            {
+                _context.Update(editedFlight);
+
+                _context.SaveChanges();
+
+                return new GeneralResponse()
+                {
+                    IsSuccess = true,
+
+                    Data = editedFlight,
+
+                    Message = "Flight Edited Successfully",
+                };
+
+                // omar : we can use one od them if we needed but I think the General Response is better 
+                #region possible return responses
+                // omar : possible return responses :
+
+                //return NoContent();
+
+                //return Created($"http://localhost:40640/api/flight{editedFlight.Id}", editedFlight);
+
+                //return CreatedAtAction("GetById", new { id = editedFlight.Id }, editedFlight); 
+                #endregion
+            }
+        }
+
+        [HttpDelete("{id:int}")] // from route
+        public ActionResult<GeneralResponse> Delete(int id)
+        {
+            Flight? flightFromDB = _context.Flights.FirstOrDefault(flight => flight.Id == id);
+
+            if (flightFromDB == null)
+            {
+                return new GeneralResponse()
+                {
+                    IsSuccess = false,
+
+                    Data = null,
+
+                    Message = "No Flight Found with this ID",
+                };
+            }
+            else
+            {
+                try
+                {
+                    _context.Remove(id);
+
+                    _context.SaveChanges();
+
+                    return new GeneralResponse()
+                    {
+                        IsSuccess = true,
+
+                        Data = null,
+
+                        Message = "Flight Edited Successfully",
+                    };
+
+                    // omar : possible return response :
+                    //return NoContent();
+                }
+                catch (Exception ex)
+                {
+                    return new GeneralResponse()
+                    {
+                        IsSuccess = false,
+
+                        Data = null,
+
+                        Message = ex.Message,
+                    };
+                }
+            }
+        }
     }
 }
