23e0647bbbda5b676403200c644aa5a9034130e8
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-05-01 10:49:22 +0300

    done with flight controller - injection Repo

--- Flight Booking System/Services/IFlightService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Flight_Booking_System.Models;
-using Flight_Booking_System.Repositories;
-
-namespace Flight_Booking_System.Services
-{
-    public interface IFlightService : IRepository<Flight>
-    {
-    }
-}
