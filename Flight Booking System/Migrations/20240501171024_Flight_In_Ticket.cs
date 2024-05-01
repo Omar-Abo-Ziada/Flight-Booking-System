de18178997ea437a0464e6a17bcdb72b6ecb6fbf
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-05-01 20:11:53 +0300

    made FK nullable in flight model and DTO

---
+++ Flight Booking System/Migrations/20240501171024_Flight_In_Ticket.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Flight_Booking_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class Flight_In_Ticket : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "FlightId",
+                table: "Tickets",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Duration",
+                table: "Flights",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tickets_FlightId",
+                table: "Tickets",
+                column: "FlightId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tickets_Flights_FlightId",
+                table: "Tickets",
+                column: "FlightId",
+                principalTable: "Flights",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tickets_Flights_FlightId",
+                table: "Tickets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tickets_FlightId",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "FlightId",
+                table: "Tickets");
+
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "Flights");
+        }
+    }
+}
