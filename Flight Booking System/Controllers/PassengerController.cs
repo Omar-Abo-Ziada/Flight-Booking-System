8c792335ab999cdb22ec557c11c2964a0a497029
Mahmoud-Mohamed-Maher <mahmoud.do7aa@gmail.com>
2024-05-12 17:26:22 +0300

    not finished

--- Flight Booking System/Controllers/PassengerController.cs
+++ Flight Booking System/Controllers/PassengerController.cs
@@ -15,10 +15,14 @@ namespace Flight_Booking_System.Controllers
     {
         readonly IPassengerRepository passengerRepository;
         readonly IMapper mapper;
-        public PassengerController(IPassengerRepository _passengerRepository, IMapper mapper)
+        private readonly IFlightRepository flightRepository;
+        private readonly ITicketRepository ticketRepository;
+        public PassengerController(IPassengerRepository _passengerRepository, IMapper mapper, IFlightRepository flightRepository, ITicketRepository ticketRepository)
         {
             this.passengerRepository = _passengerRepository;
             this.mapper = mapper;
+            this.flightRepository = flightRepository;
+            this.ticketRepository = ticketRepository;
         }
 
         [HttpGet]
@@ -30,15 +34,20 @@ namespace Flight_Booking_System.Controllers
 
             foreach (Passenger passenger in Passengers)
             {
-                //PassengerDTO passenerDTO = new PassengerDTO() 
-                //{
-                //    Id = passenger.Id,
-                //    Name = passenger.Name,
-                //    Gender = passenger.Gender,
-                //}; // the same thing but prefered using mapper
-                var passengerDTOMap = mapper.Map<Passenger, PassengerDTO>(passenger);
+                PassengerDTO passenerDTO = new PassengerDTO()
+                {
+                    Id = passenger.Id,
+                    Name = passenger.Name,
+                    Gender = passenger.Gender,
+                    Age = (int)passenger.Age,
+                    IsChild = passenger.IsChild,
+                    FlightId = passenger.FlightId,
+                    PassportNum = passenger.PassportNum,
+                    NationalId = passenger.NationalId
+                };
 
-                passengerDTOs.Add(passengerDTOMap);
+
+                passengerDTOs.Add(passenerDTO);
             }
 
             return new GeneralResponse()
@@ -69,36 +78,78 @@ namespace Flight_Booking_System.Controllers
             }
             else
             {
-                var passengerDTOMap = mapper.Map<Passenger, PassengerDTO>(passengerFromDB);
+                PassengerDTO passengerDTO = new PassengerDTO() 
+                {
+                    Id = passengerFromDB.Id,
+                    Name = passengerFromDB.Name,
+                    Gender = passengerFromDB.Gender,
+                    Age = (int)passengerFromDB.Age,
+                    IsChild = passengerFromDB.IsChild,
+                    FlightId = passengerFromDB.FlightId,
+                    PassportNum = passengerFromDB.PassportNum,
+                    NationalId = passengerFromDB.NationalId
+
+                };
 
 
                 return new GeneralResponse()
                 {
                     IsSuccess = true,
 
-                    Data = passengerDTOMap,
+                    Data = passengerDTO,
 
-                    Message = "Found"
+                    Message = "This is the Passenger you searched for "
                 };
             }
         }
 
         [HttpPost]
         //[Authorize]
-        public ActionResult<GeneralResponse> Add(Passenger passenger)
+        public ActionResult<GeneralResponse> Add(PassengerDTO passengerDto)
         {
             ///TODO : Don't forget to send the dto in the params not the model
             if (ModelState.IsValid)
             {
+                Flight? flight = flightRepository.GetWithPlane_Passengers(passengerDto.FlightId);
+
+                if (flight?.Plane?.capacity <= flight?.Passengers?.Count)
+                {
+                    return new GeneralResponse()
+                    {
+                        IsSuccess = false,
+                        Data = null,
+                        Message = "There is no place left on the plane ."
+                    };
+                }
+
+                Passenger passenger = new Passenger()
+                {
+                    Name = passengerDto.Name,
+                    Gender = passengerDto.Gender,
+                    Age = passengerDto.Age,
+                    IsChild = passengerDto.IsChild,
+                    FlightId = passengerDto.FlightId,
+                    Flight=flight,
+                    PassportNum = passengerDto.PassportNum,
+                    NationalId = passengerDto.NationalId
+
+                };
                 passengerRepository.Insert(passenger);
 
                 passengerRepository.Save();
+                Ticket ticket = ticketRepository.Get(t => t.PassengerId == passenger.Id).FirstOrDefault();
+                flight.Passengers.Add(passenger);
+                passenger.Ticket = ticket; 
+
+                passengerRepository.Save();
+                ticketRepository.Save();
+                flightRepository.Save();
 
                 return new GeneralResponse()
                 {
                     IsSuccess = true,
 
-                    Data = passenger,
+                    Data = passenger.Id,
 
                     Message = "New Passenger Added Successfully",
                 };
